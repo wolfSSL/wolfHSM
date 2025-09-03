12be2c69ceb5c60298b9d7f8ffdb277effc1677a
JacobBarthelmeh <jacob@wolfssl.com>
2025-09-03 14:56:18 -0600

    make example variables unique

--- examples/posix/wh_posix_tcp.c
+++ examples/posix/wh_posix_tcp.c
@@ -18,49 +18,49 @@
 
 /* client configuration structures */
 whTransportClientCb            pttcClientTcpCb = PTT_CLIENT_CB;
-posixTransportTcpClientContext tcc;
+posixTransportTcpClientContext tccTcp;
 posixTransportTcpConfig        mytcpconfig;
-whCommClientConfig             cc_conf;
+whCommClientConfig             ccTcpConf;
 
 /* client configuration setup example for transport */
 int wh_Client_ExampleTCPConfig(whClientConfig* c_conf)
 {
-    memset(&tcc, 0, sizeof(posixTransportTcpClientContext));
+    memset(&tccTcp, 0, sizeof(posixTransportTcpClientContext));
 
     mytcpconfig.server_ip_string = WH_SERVER_TCP_IPSTRING;
     mytcpconfig.server_port      = WH_SERVER_TCP_PORT;
 
-    cc_conf.transport_cb      = &pttcClientTcpCb;
-    cc_conf.transport_context = (void*)&tcc;
-    cc_conf.transport_config  = (void*)&mytcpconfig;
-    cc_conf.client_id         = WH_CLIENT_ID;
-    c_conf->comm              = &cc_conf;
+    ccTcpConf.transport_cb      = &pttcClientTcpCb;
+    ccTcpConf.transport_context = (void*)&tccTcp;
+    ccTcpConf.transport_config  = (void*)&mytcpconfig;
+    ccTcpConf.client_id         = WH_CLIENT_ID;
+    c_conf->comm              = &ccTcpConf;
 
     return WH_ERROR_OK;
 }
 
 /* server configuration structures */
 whTransportServerCb            pttServerTcpCb = PTT_SERVER_CB;
-posixTransportTcpServerContext tsc;
+posixTransportTcpServerContext tscTcp;
 posixTransportTcpConfig        mytcpconfig;
-whCommServerConfig             cs_conf;
+whCommServerConfig             csTcpConf;
 
 /* Server configuration setup example for transport
  * Does not setup flash, nvm, crypto, she, etc. */
 int wh_Server_ExampleTCPConfig(whServerConfig* s_conf)
 {
     /* Server configuration/context */
-    memset(&tsc, 0, sizeof(posixTransportTcpServerContext));
+    memset(&tscTcp, 0, sizeof(posixTransportTcpServerContext));
 
     mytcpconfig.server_ip_string = WH_SERVER_TCP_IPSTRING;
     mytcpconfig.server_port      = WH_SERVER_TCP_PORT;
 
-    cs_conf.transport_cb      = &pttServerTcpCb;
-    cs_conf.transport_context = (void*)&tsc;
-    cs_conf.transport_config  = (void*)&mytcpconfig;
-    cs_conf.server_id         = WH_SERVER_ID;
+    csTcpConf.transport_cb      = &pttServerTcpCb;
+    csTcpConf.transport_context = (void*)&tscTcp;
+    csTcpConf.transport_config  = (void*)&mytcpconfig;
+    csTcpConf.server_id         = WH_SERVER_ID;
 
-    s_conf->comm_config = &cs_conf;
+    s_conf->comm_config = &csTcpConf;
 
     return WH_ERROR_OK;
 }
