b0f5a7cdade54e828af27872ed2b690be2229a51
jackctj117 <jackctj117@gmail.com>
2025-09-23 14:33:12 -0600

    Change clang-tidy-builder script to ingnore .clang-tidy and CLANG_TIDY_ARGS

--- tools/static-analysis/clang-tidy-builder.sh
+++ tools/static-analysis/clang-tidy-builder.sh
@@ -5,7 +5,7 @@ shopt -s extglob || exit $?
 
 retval=0
 
-if [[ -n "${CLANG_TIDY:-}" ]]; then
+if [[ -v CLANG_TIDY ]]; then
 
     while :; do
 
@@ -17,32 +17,18 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 	done
 	unset arg
 
-	if [[ -z "${source_file:-}" ]]; then
+	if [[ ! -v source_file ]]; then
 	    retval=0
 	    break
 	fi
 
-	# Skip clang-tidy analysis for wolfSSL dependency files
-	# Only analyze wolfHSM source code (src/ and wolfhsm/ directories)
-	if [[ "$source_file" == */wolfssl/* ]]; then
-	    retval=0
-	    break
+	if [[ -v CLANG_TIDY_ARGS ]]; then
+	    read -r -a clang_tidy_args_array < <(echo "$CLANG_TIDY_ARGS") || exit $?
+	else
+	    clang_tidy_args_array=()
 	fi
 
-    # Only analyze wolfHSM sources: src/ and wolfhsm/ (skip others, e.g., test/, tools/, wolfssl/)
-    if [[ ! "$source_file" == */src/* && ! "$source_file" == */wolfhsm/* ]]; then
-        retval=0
-        break
-    fi
-
-    # Always declare the array (avoid unbound errors under set -u)
-    declare -a clang_tidy_args_array=()
-    # Build argument array only if CLANG_TIDY_ARGS is present
-    if [[ -n "${CLANG_TIDY_ARGS:-}" ]]; then
-        read -r -a clang_tidy_args_array < <(echo "${CLANG_TIDY_ARGS}") || exit $?
-    fi
-
-	if [[ -n "${CLANG_TIDY_PER_FILE_CHECKS:-}" ]]; then
+	if [[ -v CLANG_TIDY_PER_FILE_CHECKS ]]; then
 	    per_file_checks=()
 	    read -r -a clang_tidy_per_file_checks < <(echo "$CLANG_TIDY_PER_FILE_CHECKS") || exit $?
 	    for check in "${clang_tidy_per_file_checks[@]}"; do
@@ -53,7 +39,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 	    unset check
 	fi
 
-	if [[ -n "${per_file_checks:-}" ]]; then
+	if [[ -v per_file_checks ]]; then
 	    declare -i i=0
 	    while [[ $i -lt ${#clang_tidy_args_array[@]} ]]; do
 		if [[ "${clang_tidy_args_array[i]}" =~ ^-checks ]]; then
@@ -66,7 +52,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 		fi
 		: $((++i))
 	    done
-	    if [[ -z "${added_to_existing_checks:-}" ]]; then
+	    if [[ ! -v added_to_existing_checks ]]; then
 		SAVE_IFS="$IFS"
 		IFS=,
 		clang_tidy_args_array+=("-checks=${per_file_checks[*]}")
@@ -74,7 +60,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 	    fi
 	fi
 
-	if [[ -n "${CLANG_TIDY_PER_FILE_ARGS:-}" ]]; then
+	if [[ -v CLANG_TIDY_PER_FILE_ARGS ]]; then
 	    read -r -a clang_tidy_per_file_args < <(echo "$CLANG_TIDY_PER_FILE_ARGS") || exit $?
 	    for arg in "${clang_tidy_per_file_args[@]}"; do
 		if [[ "$source_file" =~ ${arg%:*} ]]; then
@@ -84,26 +70,24 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 	    unset arg
 	fi
 
-	if [[ -n "${CLANG_TIDY_CONFIG:-}" ]]; then
+	if [[ -v CLANG_TIDY_CONFIG ]]; then
 	    clang_tidy_args_array+=("-config=${CLANG_TIDY_CONFIG}")
 	fi
 
-	if [[ -n "${CLANG_TIDY_EXTRA_ARGS:-}" ]]; then
+	if [[ -v CLANG_TIDY_EXTRA_ARGS ]]; then
 	    read -r -a clang_tidy_extra_args < <(echo "$CLANG_TIDY_EXTRA_ARGS") || exit $?
 	    clang_tidy_args_array+=("${clang_tidy_extra_args[@]}")
 	fi
 
-    if [[ ${#clang_tidy_args_array[@]} -gt 0 ]]; then
-        for arg in "${clang_tidy_args_array[@]}"; do
+	for arg in "${clang_tidy_args_array[@]}"; do
 	    case "$arg" in
 		--use-color) use_color=
 			     ;;
 	    esac
-        done
-    fi
+	done
 	unset arg
 
-	if [[ -n "${use_color:-}" ]]; then
+	if [[ -v use_color ]]; then
 	    if text_normal_start="$(tput sgr0)"; then
 		do_style_restore=
 	    fi
@@ -113,19 +97,19 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 	    case "$clang_tidy_line" in
 		Use\ -header-filter=.*\ to\ display\ errors\ from\ all\ non-system\ headers.\ Use\ -system-headers\ to\ display\ errors\ from\ system\ headers\ as\ well.)
 
-		    [[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+		    [[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    ;;
 
 		+([0-9])\ warning?(s)\ generated.)
 
-		    [[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+		    [[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    ;;
 
 		Suppressed\ +([0-9])\ warnings\ \(+([0-9])\ NOLINT\).)
 
 		    IFS="[( ]" read -r -a clang_tidy_line_a < <(echo "$clang_tidy_line")
 		    if [[ "${clang_tidy_line_a[3]}" == "${clang_tidy_line_a[1]}" ]]; then
-			[[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+			[[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    else
 			echo "$clang_tidy_line" >&2
 		    fi
@@ -135,7 +119,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 
 		    IFS="[( ]" read -r -a clang_tidy_line_a < <(echo "$clang_tidy_line")
 		    if [[ $((clang_tidy_line_a[3] + clang_tidy_line_a[7])) == "${clang_tidy_line_a[1]}" ]]; then
-			[[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+			[[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    else
 			echo "$clang_tidy_line" >&2
 		    fi
@@ -145,7 +129,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 
 		    IFS="[( ]" read -r -a clang_tidy_line_a < <(echo "$clang_tidy_line")
 		    if [[ "${clang_tidy_line_a[3]}" == "${clang_tidy_line_a[1]}" ]]; then
-			[[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+			[[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    else
 			echo "$clang_tidy_line" >&2
 		    fi
@@ -155,7 +139,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 
 		    IFS="[( ]" read -r -a clang_tidy_line_a < <(echo "$clang_tidy_line")
 		    if [[ $((clang_tidy_line_a[3] + clang_tidy_line_a[7])) == "${clang_tidy_line_a[1]}" ]]; then
-			[[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+			[[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    else
 			echo "$clang_tidy_line" >&2
 		    fi
@@ -165,7 +149,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 
 		    IFS="[( ]" read -r -a clang_tidy_line_a < <(echo "$clang_tidy_line")
 		    if [[ $((clang_tidy_line_a[3] + clang_tidy_line_a[7] + clang_tidy_line_a[9])) == "${clang_tidy_line_a[1]}" ]]; then
-			[[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+			[[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    else
 			echo "$clang_tidy_line" >&2
 		    fi
@@ -175,7 +159,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 
 		    IFS="[( ]" read -r -a clang_tidy_line_a < <(echo "$clang_tidy_line")
 		    if [[ "${clang_tidy_line_a[3]}" == "${clang_tidy_line_a[1]}" ]]; then
-			[[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+			[[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    else
 			echo "$clang_tidy_line" >&2
 		    fi
@@ -185,7 +169,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 
 		    IFS="[( ]" read -r -a clang_tidy_line_a < <(echo "$clang_tidy_line")
 		    if [[ $((clang_tidy_line_a[3] + clang_tidy_line_a[7])) == "${clang_tidy_line_a[1]}" ]]; then
-			[[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+			[[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    else
 			echo "$clang_tidy_line" >&2
 		    fi
@@ -195,7 +179,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 
 		    IFS="[( ]" read -r -a clang_tidy_line_a < <(echo "$clang_tidy_line")
 		    if [[ $((clang_tidy_line_a[3] + clang_tidy_line_a[7])) == "${clang_tidy_line_a[1]}" ]]; then
-			[[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+			[[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    else
 			echo "$clang_tidy_line" >&2
 		    fi
@@ -205,7 +189,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 
 		    IFS="[( ]" read -r -a clang_tidy_line_a < <(echo "$clang_tidy_line")
 		    if [[ $((clang_tidy_line_a[3] + clang_tidy_line_a[7] + clang_tidy_line_a[12])) == "${clang_tidy_line_a[1]}" ]]; then
-			[[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+			[[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    else
 			echo "$clang_tidy_line" >&2
 		    fi
@@ -215,7 +199,7 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 
 		    IFS="[( ]" read -r -a clang_tidy_line_a < <(echo "$clang_tidy_line")
 		    if [[ "${clang_tidy_line_a[1]}" == "${clang_tidy_line_a[3]}" ]]; then
-			[[ -n "${do_style_restore:-}" ]] && echo -n "$text_normal_start" >&2
+			[[ -v do_style_restore ]] && echo -n "$text_normal_start" >&2
 		    else
 			echo "$clang_tidy_line" >&2
 		    fi
@@ -229,9 +213,9 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 
             esac
 
-        done < <("$CLANG_TIDY" ${clang_tidy_args_array+"${clang_tidy_args_array[@]}"} "$source_file" -- "$@" 2>&1)
+        done < <("$CLANG_TIDY" "${clang_tidy_args_array[@]}" "$source_file" -- "$@" 2>&1)
 
-	if [[ "$retval" != '0' && -n "${do_style_restore:-}" ]]; then
+	if [[ "$retval" != '0' && -v do_style_restore ]]; then
 	    echo -n "$text_normal_start" >&2
 	fi
 	break
@@ -239,13 +223,15 @@ if [[ -n "${CLANG_TIDY:-}" ]]; then
 fi
 
 if [[ "$retval" != '0' ]]; then
-    exit "$retval"
+    if [[ -v CLANG_TIDY_STATUS_FILE ]]; then
+	# shellcheck disable=SC2320 # noise
+	echo "${source_file} ${retval}" >> "$CLANG_TIDY_STATUS_FILE" || exit $?
+    else
+	exit "$retval"
+    fi
 fi
 
-# shellcheck disable=SC2162 # intentional use of read with process substitution; -r flag is present and safe.
-if [[ -n "${CLANG_OVERRIDE_CFLAGS:-}" ]]; then
-    read -r -a CLANG_OVERRIDE_CFLAGS_a < <(echo "${CLANG_OVERRIDE_CFLAGS}")
-    exec "$CLANG" "$@" "${CLANG_OVERRIDE_CFLAGS_a[@]}"
-else
-    exec "$CLANG" "$@"
-fi
+# shellcheck disable=SC2162 # we want backslashes to be interpreted here.
+read -a CLANG_OVERRIDE_CFLAGS_a < <(echo "${CLANG_OVERRIDE_CFLAGS-}")
+
+exec "$CLANG" "$@" "${CLANG_OVERRIDE_CFLAGS_a[@]}"
