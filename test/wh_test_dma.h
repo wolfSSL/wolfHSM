d7221b4ebdb46ee95dcccf71a1b8ccae9954cf2b
Sameeh Jubran <sameeh@wolfssl.com>
2026-03-12 15:44:19 +0200

    Fix integer overflow in DMA allow list boundary check
    Add overflow guards to _checkAddrAgainstAllowList to prevent
    address-space wraparound from bypassing the allow list on 32-bit
    and 64-bit systems. Reject requests where addr + size overflows,
    and skip allow list entries whose own range overflows.
    
    Signed-off-by: Sameeh Jubran <sameeh@wolfssl.com>

---
+++ test/wh_test_dma.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2026 wolfSSL Inc.
+ *
+ * This file is part of wolfHSM.
+ *
+ * wolfHSM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfHSM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with wolfHSM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * test/wh_test_dma.h
+ *
+ * DMA allow list boundary check tests
+ */
+#ifndef TEST_WH_TEST_DMA_H_
+#define TEST_WH_TEST_DMA_H_
+
+int whTest_Dma(void);
+
+#endif /* TEST_WH_TEST_DMA_H_ */
